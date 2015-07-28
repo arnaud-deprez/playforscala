587641a16086e554cc1955fe57825c883f814f63
Arnaud Deprez <arnaud.deprez@lampiris.be>
2015-07-28 22:12:10 +0200

    save application products

---
+++ products/app/controllers/BarCodes.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import javax.inject.Inject
+
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller}
+
+/**
+ * Created by arnaud.deprez on 27/07/15.
+ */
+class BarCodes @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  val ImageResolution = 144
+
+  def barcode(ean: Long) = Action {
+    val MimeType = "image/png"
+    try {
+      val imageData = ean13BarCode(ean, MimeType)
+      Ok(imageData).as(MimeType)
+    }
+    catch {
+      case e: IllegalArgumentException => BadRequest("Couldn’t generate bar code. Error: " + e.getMessage)
+    }
+  }
+
+  def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
+
+    import java.io.ByteArrayOutputStream
+    import java.awt.image.BufferedImage
+    import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
+    import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+
+    val output: ByteArrayOutputStream = new ByteArrayOutputStream
+    val canvas: BitmapCanvasProvider = new BitmapCanvasProvider(output, mimeType, ImageResolution,
+        BufferedImage.TYPE_BYTE_BINARY, false, 0)
+    val barcode = new EAN13Bean()
+    barcode.generateBarcode(canvas, String valueOf ean)
+    canvas.finish
+
+    output.toByteArray
+  }
+}
--- products/app/controllers/Products.scala
+++ products/app/controllers/Products.scala
@@ -2,17 +2,59 @@ package controllers
 
 import javax.inject.Inject
 
-import play.api.i18n.{MessagesApi, I18nSupport}
-import play.api.mvc.{Action, Controller}
 import models.Product
+import play.api.i18n.{Messages, I18nSupport, MessagesApi}
+import play.api.mvc.{Flash, Action, Controller}
+import play.api.data._
+import play.api.data.Forms._
 
 /**
-  * Created by arnaud.deprez on 22/07/15.
+ * Created by arnaud.deprez on 22/07/15.
  */
 class Products @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  var products: Set[Product] = Set()
+
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply)
+  )
 
   def list = Action { implicit request =>
     val products = Product.findAll
     Ok(views.html.products.list(products))
+  }
+
+  def show(ean: Long) = Action { implicit request =>
+    Product.findByEan(ean).map { product =>
+      Ok(views.html.products.details(product))
+    }.getOrElse(NotFound)
+  }
+
+  def save = Action { implicit request =>
+    val newProductForm: Form[Product] = productForm.bindFromRequest()
+
+    newProductForm.fold(
+      hasErrors = { form =>
+        Redirect(routes.Products.newProduct())
+          .flashing(Flash(form.data) + ("error" -> Messages("validation.errors")))
+      },
+      success = { newProduct =>
+        Product.add(newProduct)
+        val message = Messages("products.new.success", newProduct.name)
+        Redirect(routes.Products.show(newProduct.ean)).
+          flashing("success" -> message)
+      })
+  }
+
+  def newProduct = Action { implicit request =>
+    val form = if (request.flash.get("error").isDefined)
+      productForm.bind(request.flash.data)
+    else
+      productForm
+    Ok(views.html.products.edit(form))
   }
 }
